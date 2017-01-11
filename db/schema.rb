5425767778c31c9d2e4dba879074589828097a4d
fanliangchun <jeremydo2010@gmail.com>
2017-01-11 18:49:46 +0800

    implement part4

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111103208) do
+ActiveRecord::Schema.define(version: 20170111104225) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.integer  "wage_upper_bount"
     t.integer  "wage_lower_bound"
     t.integer  "wage_upper_bound"
     t.string   "contact_email"
+    t.boolean  "is_hidden",        default: true
   end
 
   create_table "users", force: :cascade do |t|
